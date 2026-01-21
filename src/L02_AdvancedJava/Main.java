c730440a26a9ef6da426cc2cb441e70b1587a334
thenghiabk <thenghiabk@gmail.com>
2026-01-21 16:29:10 +0700

    added AdvancedJava

--- src/L02_AdvancedJava/Main.java
+++ src/L02_AdvancedJava/Main.java
@@ -105,61 +105,30 @@ class Student extends User {
 public class Main {
 	public static void main (String[] args) {
 
+		List<User> users = new ArrayList<>();
+
 		User guest1 = new User("Lucy", "222",  "Guest");
 		System.out.println(guest1.getInfo());
 
-		Admin admin1 = new Admin("David", "111",  "Admin", "dav02@fpt.com", 5000);
+		User admin1 = new Admin("David", "111",  "Admin", "dav02@fpt.com", 5000);
 		System.out.println(admin1.getInfo());
 
-		Teacher teacher1 = new Teacher("John","000", "Teacher", "joh01@fpt.com", "Math", "English");
+		User teacher1 = new Teacher("John","000", "Teacher", "joh01@fpt.com", "Math", "English");
 		System.out.println(teacher1.getInfo());
 
-		Student student1 = new Student("Ben", "333", "Student", "ben3@fpt.edu.vn", "Math", "English", "Physics");
+		User student1 = new Student("Ben", "333", "Student", "ben3@fpt.edu.vn", "Math", "English", "Physics");
 		System.out.println(student1.getInfo());
 
-		List<IPerson> users = new ArrayList<>();
+		users.add(guest1);
 		users.add(admin1);
 		users.add(teacher1);
+		users.add(student1);
+
+		// TODO
 
 
 
 
-
-
-		// String[] names = new String[10];
-		// String[] telephones = new String[10];
-		// String[] emails = new String[10];
-		// String[] roles = new String[10];
-		// String[][] subjects = new String[10][3];
-		//
-		// // add a new user
-		// names[0] = "John";
-		// telephones[0] = "000";
-		// emails[0] = "joh01@fpt.com";
-		// roles[0] = "Teacher";
-		// subjects[0][0] = "Math";
-		// subjects[0][1] = "English";
-		// subjects[0][2] = "Physic";
-		//
-		// // add a new user
-		// names[1] = "David";
-		// telephones[1] = "111";
-		// emails[1] = "dav02@fpt.com";
-		// roles[1] = "Admin";
-		//
-		// // add a new user
-		// names[2] = "Lucy";
-		// telephones[2] = "222";
-		// roles[2] = "Guest";
-		//
-		// // add a new user
-		// names[3] = "Ben";
-		// telephones[3] = "333";
-		// emails[3] = "ben03@fpt.edu.vn";
-		// roles[3] = "Student";
-		// subjects[3][0] = "Math";
-		// subjects[3][1] = "English";
-		// subjects[3][2] = "Physic";
 
 	}
 }
